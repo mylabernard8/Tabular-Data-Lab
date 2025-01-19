d36c681cebdb00aa7245561b1dbdb7c3b2ccaa53
mylabernard8 <myla.bernard26@avenues.org>
2025-01-19 15:53:36 -0500

    Just setting up code for sketch, using original demo but got rid of table for the line graph

--- sketch1.js
+++ sketch1.js
@@ -1,1 +1,12 @@
+const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv";
+let weatherTable; //variable for the weather data
+
+function preload() {
+  weatherTable = loadTable(weatherURL, 'csv', 'header');
+}
+
+function setup() {
+  createCanvas(800, 400); 
+  noLoop();
+}
 
