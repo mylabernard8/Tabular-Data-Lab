3b6880495b87d6a33f547aeb5e236d3abe653c1a
mylabernard8 <myla.bernard26@avenues.org>
2025-01-20 20:56:27 -0500

    Deleted previous code because I realized that I was making the graph so restarted with this, loading and setting up everything

--- sketch2.js
+++ sketch2.js
@@ -1,12 +1,19 @@
 const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv";
 let weatherTable;
+let currentSubset = "January";
+
+const subsets = {
+  "January": { start: 0, end: 30 },
+  "February": { start: 31, end: 59 },
+  "March": { start: 60, end: 90 },
+};
 
 function preload() {
-  weatherTable = loadTable(weatherURL, 'csv', 'header');
+  weatherTable = loadTable(weatherURL, 'csv', 'header'); // Load data
 }
 
 function setup() {
-  createCanvas(800, 400);
-  noLoop();
+  createCanvas(800, 400); 
+  noLoop(); 
 }
 
